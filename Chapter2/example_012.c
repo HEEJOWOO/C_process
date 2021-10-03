ffebfb807e995f69ee07823842785cd8fa5167a8
HEEJO WOO <61686244+HEEJOWOO@users.noreply.github.com>
2021-10-03 20:22:29 +0900

    Create example_012.c

---
+++ Chapter2/example_012.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+main() {
+	int ch_1;
+	unsigned int ch_2;
+	int ch_3;
+
+	ch_1 = 2000000000;
+	ch_2 = 4000000000;
+	ch_3 = 'a';
+	printf("ch_1 은 %d 값 입니다.\n", ch_1);
+	printf("ch_2 은 %u 값 입니다.\n", ch_2);
+	printf("ch_3 은 %d 값 입니다.\n", ch_3);
+}
