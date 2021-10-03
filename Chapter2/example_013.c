126e05d4fb6c190f682c381a4e2386b2a011ffbf
HEEJO WOO <61686244+HEEJOWOO@users.noreply.github.com>
2021-10-03 20:35:15 +0900

    Create example_013.c

---
+++ Chapter2/example_013.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+main() {
+	float ch_1;
+	double ch_2;
+	double ch_3;
+
+	ch_1 = 3.141592;
+	ch_2 = 1234567890;
+	ch_3 = 'a';
+
+	printf("ch_1의 값은 %f 입니다.\n", ch_1);
+	printf("ch_1의 값은 %E 입니다.\n", ch_2);
+	printf("ch_1의 값은 %E 입니다.\n", ch_3);
+}
