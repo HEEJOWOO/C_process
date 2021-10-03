b1c0eb7cc54a571499cce751c731141fb0c9dad6
HEEJO WOO <61686244+HEEJOWOO@users.noreply.github.com>
2021-10-03 20:17:57 +0900

    Add files via upload

---
+++ Chapter2/example_011.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+main() {
+	char ch_1;
+	unsigned char ch_2;
+	char ch_3;
+	
+	ch_1 = 200;
+	ch_2 = 200;
+	ch_3 = 'a';
+	
+	printf("ch_1의 값은 %d 입니다.\n", ch_1);
+	printf("ch_2의 값은 %d 입니다.\n", ch_2);
+	printf("ch_3의 값은 %d 입니다.\n", ch_3);
+	
+}
