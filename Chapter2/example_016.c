551f2c8710fe45150b825a20e445849a0ab3602b
HEEJO WOO <61686244+HEEJOWOO@users.noreply.github.com>
2021-10-04 16:03:21 +0900

    Create example_016.c

---
+++ Chapter2/example_016.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#define HUNDRED 100
+const char j = 10;
+main() {
+	//HUNDRED = 200; //상수는 변경할 수 없음
+	//j = 200; //상수는 변경할 수 없음
+	printf("HUNDRED의 값은 %d 입니다.\n", HUNDRED);
+	printf("j의 값은 %d 입니다.\n", j);
+	
+}
