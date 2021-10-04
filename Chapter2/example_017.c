def773e18b6a60d3f19a3a9bb6862695c2741cfe
HEEJO WOO <61686244+HEEJOWOO@users.noreply.github.com>
2021-10-04 16:08:56 +0900

    Create example_017.c

---
+++ Chapter2/example_017.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#define HUNTHREAD_THOUSAND 100000
+const int j = 200000;
+main() {
+	printf("HUNTHREAD_THOUSAND의 값은 %d 입니다.\n", HUNTHREAD_THOUSAND);
+	printf("j의 값은 %d 입니다.\n", j);
+}
