a6fdafc3c1149163241b57a86cc0bbf34aad6af9
HEEJO WOO <61686244+HEEJOWOO@users.noreply.github.com>
2021-10-04 17:23:24 +0900

    Create example_026.c

---
+++ Chapter2/example_026.c
@@ -0,0 +1,11 @@
+//논리 연산자 이해
+#include<stdio.h>
+main() {
+	int x = 1;
+	int y = 2;
+	
+	if (x > 0 && x < 10) printf("x>0 && x<10\n");
+	if (x <0 || y<10) printf("x <0 || y<10\n");
+	if (!(x>y)) printf("!(x<y)\n");
+	
+}
