6a231ad0352a652dc0f1ca8719c5bfdf730fb8e3
HEEJO WOO <61686244+HEEJOWOO@users.noreply.github.com>
2021-10-04 16:21:17 +0900

    Create example_018.c

---
+++ Chapter2/example_018.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#define PI 3.141592
+const float j = 1.23456789;
+
+main() {
+	
+	printf("PI의 값은 %f 입니다.\n", PI); //%f는 7자리까지 표현, 7자리 이하의 값은 표현하지 않음
+	printf("j의 값은 %f 입니다.\n", j);
+	
+	
+}
