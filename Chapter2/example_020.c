e5670435416cbd10bd840579edff0cc72599a6cd
HEEJO WOO <61686244+HEEJOWOO@users.noreply.github.com>
2021-10-04 16:35:25 +0900

    Create example_020.c

---
+++ Chapter2/example_020.c
@@ -0,0 +1,14 @@
+//문자열형 상수 
+#include<stdio.h>
+#define KOREA "대한민국"
+#define BOOK "This is a book"
+const char* SOCCER = "축구";
+
+main() {
+
+	printf("KOREA의 값은 %s 입니다.\n", KOREA);
+	printf("BOOK의 값은 %s 입니다.\n", BOOK);
+	printf("SOCCER의 값은 %s 입니다.\n", SOCCER);
+
+
+}
