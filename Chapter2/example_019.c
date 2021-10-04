920421c3be4450a0181622e2f646e7dae4c881ca
HEEJO WOO <61686244+HEEJOWOO@users.noreply.github.com>
2021-10-04 16:27:34 +0900

    Create example_019.c

---
+++ Chapter2/example_019.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#define TRUE 1
+#define FALSE 0
+main() {
+	if (TRUE) printf("TRUE는 찹입니다.\n");
+	else printf("FALSE는 거짓입니다.\n");
+
+	if (FALSE) printf("FALSE는 찹입니다.\n");
+	else printf("FLASE는 거짓입니다.\n");
+
+}
