0b540f6763aa5b6eeea6019d5d89cb489ae0be6e
HEEJO WOO <61686244+HEEJOWOO@users.noreply.github.com>
2021-10-04 17:17:05 +0900

    Create example_025.c

---
+++ Chapter2/example_025.c
@@ -0,0 +1,16 @@
+//관계 연산자 이해
+#include<stdio.h>
+main() {
+	int x = 1;
+	int y = 2;
+	
+
+	if (x==y) printf("x == y\n"); 
+	if (x!=y) printf("x != y\n");
+	if (x <= y) printf("x <= y\n");
+	if (x >= y) printf("x >= y\n");
+	if (x < y) printf("x < y\n");
+	if (x > y) printf("x > y\n");
+	
+
+}
