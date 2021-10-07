3ae3ed7aa014fa5a90fb82c5f6cc13a3f493ea19
HEEJO WOO <61686244+HEEJOWOO@users.noreply.github.com>
2021-10-07 10:17:28 +0900

    Create example_034.c

---
+++ Chapter2/example_034.c
@@ -0,0 +1,16 @@
+//조건 선택문
+#include<stdio.h>
+main() {
+	int i = 5;
+	switch (i) {
+	case 1:
+		printf("%d\n", i);
+		break;
+	case 2:
+		printf("%d\n", i);
+		break;
+	default:
+		printf("default : %d\n", i);
+		break;
+	}
+}
