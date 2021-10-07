982ce39ca47e06648a80f9f69cd19db6c73dd8b9
HEEJO WOO <61686244+HEEJOWOO@users.noreply.github.com>
2021-10-07 14:15:02 +0900

    Create example_045.c

---
+++ Chapter2/example_045.c
@@ -0,0 +1,15 @@
+//열거형 이해
+#include<stdio.h>
+enum {sun=0,mon,tue,wed,thr,fri,sat};
+
+main() {
+
+	printf("%d\n", sun);
+	printf("%d\n", mon);
+	printf("%d\n", tue);
+	printf("%d\n", tue);
+	printf("%d\n", wed);
+	printf("%d\n", thr);
+	printf("%d\n", fri);
+	printf("%d\n", sat);
+}
