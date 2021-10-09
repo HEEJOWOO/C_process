b488d1c1923dd17434500cbc13240dccb1f63c8f
HEEJO WOO <61686244+HEEJOWOO@users.noreply.github.com>
2021-10-09 14:07:22 +0900

    Create example_061.c

---
+++ Chapter3/example_061.c
@@ -0,0 +1,20 @@
+//문자열 일정 크기 비교, strncmp
+#include<stdio.h>
+#include<string.h>
+
+#define SKY "sky"
+
+void main(void) {
+	char str[100];
+	int ret;
+	do {
+		gets(str);
+		ret = strcmp(str, SKY, 3);
+		if (ret == 0) {
+			printf("%3.3s==%s\n", str, SKY);
+			break;
+		}
+		else if (ret < 0) printf("%3.3s<%s\n", str, SKY);
+		else printf("%3.3s>%s\n", str, SKY);
+	} while (1);
+}
