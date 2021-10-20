359d0002a7cd6c4b79310b5ed03ed5bde5d21cc9
HEEJO WOO <61686244+HEEJOWOO@users.noreply.github.com>
2021-10-20 13:13:50 +0900

    Create example_107.c

---
+++ Chapter3/example_107.c
@@ -0,0 +1,22 @@
+//포인터를 함수에서 사용하기
+#include<stdio.h>
+
+void change_x1(int x1);
+void change_x2(int *x1);
+
+void main(void) {
+	int x = 5;
+	change_x1(x);
+	printf("%d\n", x);
+
+	change_x2(&x);
+	printf("%d\n", x);
+}
+
+void change_x1(int x1) {
+	x1 = 100;
+}
+
+void change_x2(int *x1) {
+	*x1 = 200;
+}
